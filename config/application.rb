32ef58239a27c3a372ab44e70b849201f5e769ea
Sam <samanthakpower@gmail.com>
2017-01-08 02:19:59 -0500

    Update CORS config

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ShotgunApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
   end
